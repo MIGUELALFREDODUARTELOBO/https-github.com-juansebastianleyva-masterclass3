54c93306728adc505681400f5f9c889a9b7f23ec
juansebastianleyva <jnleyva@gmail.com>
2021-10-16 18:43:34 -0500

    Msg animo!

--- src/main/java/co/usa/ciclo3/ciclo3/Ciclo3Application.java
+++ src/main/java/co/usa/ciclo3/ciclo3/Ciclo3Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @EntityScan(basePackages = {"co.usa.ciclo3.ciclo3.model"})
 @SpringBootApplication
 public class Ciclo3Application {
-
+//TODOS A SUBIR RETO 3 Y 4!!!
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo3Application.class, args);
 	}
